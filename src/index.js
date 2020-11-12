b22525bc2691755dddc141d07e11f4a6fbb18fbb
Son Beomsoo <k1a11220@.github.com>
2020-11-12 21:35:22 +0900

    Add firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import firebase from "./firebase";
+console.log(firebase);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
