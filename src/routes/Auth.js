b18a0c7b13b4379e7c0ad45f6ca6f53d88e876ac
Son Beomsoo <k1a11220@.github.com>
2020-11-13 22:56:09 +0900

    Router setup

---
+++ src/routes/Auth.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+const Auth = () => <span>Auth</span>;
+
+export default Auth;
---
+++ src/routes/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+const Home = () => <span>Home</span>;
+export default Home;
