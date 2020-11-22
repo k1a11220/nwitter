cf2f4c5e98204d2a4fe408f139baa427b7b35ecb
Beomsoo Son <k1a11220.noreply@github.com>
2020-11-22 23:52:15 +0900

    Add login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
--- src/routes/EditProfile.js
+++ src/routes/EditProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = () => {
+const EditProfile = () => {
   return <div>dd</div>;
 };
 
-export default Home;
+export default EditProfile;
